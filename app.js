3d39360f6869604d1a314d76b18d1835d9359a3b
Mette Hellemarck <mettehellemarck@hotmail.com>
2020-10-04 19:25:29 +0200

    cors stuff

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
-
+const cors = require('cors');
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 
-io.origins(['https://mh-jsramverk.me:80']);
+app.use(cors());
+
+io.origins(['https://mh-jsramverk.me:443']);
 
 io.on('connection', function (socket) {
     socket.on('user connected', function(user) {
