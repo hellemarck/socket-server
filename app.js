38c6734a04b97b2830413f1fb08083cb43e7a06a
Mette Hellemarck <mettehellemarck@hotmail.com>
2020-10-04 17:15:25 +0200

    change to public server api

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 
+io.origins(['https://socket-client.mh-jsramverk.me:443']);
 
 io.on('connection', function (socket) {
     socket.on('user connected', function(user) {
